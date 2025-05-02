0ddb83bf28dd590d597563c376a233416672a9c2
jessicaagoulart <jessicaagoulart@gmail.com>
2025-05-02 15:10:35 -0300

    feat: update env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	env: {
-		NEXT_PUBLIC_EMAILJS_SERVICE_ID: "service_0y55jal",
-		NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: "template_1jw4vgm",
-		NEXT_PUBLIC_EMAILJS_USER_ID: "gOsuwJFrtgLr1sN7c",
-	},
 };
 
 module.exports = nextConfig;
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
 				process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
 				process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
 				templateParams,
-				process.env.NEXT_PUBLIC_EMAILJS_USER_ID!
+				process.env.NEXT_PUBLIC_EMAILJS_USER_ID
 			);
 
 			alert("Message sent successfully!");
